2587dc56e2777d451696ff5cdeb6e1f5287275be
Federico Minaya <federico.minaya@aerolab.co>
2022-11-01 21:17:45 -0300

    feat: added title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,11 +2,17 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 export default function Index() {
   return (
-    <div className="bg-primary flex text-center text-alt flex-col gap-4 md:gap-10 px-2 items-center justify-center min-h-screen w-screen">
-      <h1 className="text-5xl text-alt font-serif">
-        <span>Hi 👋🏾 i'am </span>
-        <span className="font-medium">Federico Minaya </span>
-      </h1>
+    <div className="bg-primary flex text-center text-alt flex-col gap-6 md:gap-10 px-2 items-center justify-center min-h-screen w-screen">
+      <div className="space-y-4">
+        <h1 className="text-5xl text-alt font-serif">
+          <span>Hi 👋🏾 i&apos;am </span>
+          <span className="font-medium">Federico Minaya </span>
+        </h1>
+
+        <h2 className="text-3xl font-medium font-serif">
+          Full Stack Developer
+        </h2>
+      </div>
 
       <div className="text-3xl flex gap-4">
         👉🏾
