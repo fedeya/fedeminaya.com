de4c09adacdd4809bcf8fd8c0771cd841554dac0
Federico Minaya <elfedeminaya@gmail.com>
2023-03-14 18:12:12 -0300

    feat: add cache to sitemap

--- apps/web/app/lib/sitemap.server.ts
+++ apps/web/app/lib/sitemap.server.ts
@@ -1,5 +1,8 @@
 import { createSitemapGenerator } from 'remix-sitemap';
 
 export const { isSitemapUrl, sitemap } = createSitemapGenerator({
-  siteUrl: 'https://fedeminaya.com'
+  siteUrl: 'https://fedeminaya.com',
+  headers: {
+    'Cache-Control': 'public, max-age=600, s-maxage=86400'
+  }
 });
